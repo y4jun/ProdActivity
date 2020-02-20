aef3601816ef5995bb80806ccf242279e7060f18
Yongho Jun <y4jun@ucsd.edu>
2020-02-20 14:46:59 -0800

    points calculation & calendar

--- public/js/prod-activity.js
+++ public/js/prod-activity.js
@@ -2,17 +2,24 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$('#datepicker').each(function(){
+		$(this).datepicker();
+	})
 })
 
 /*
  * Function that is called when the document is ready.
  */
 var profilePoints = 0;
+var pointsEarned = 0;
+var pts = 0;
 function initializePage() {	
 	$('.assignment a').click(moreInfo);
-	$('.hideInfo #complete').click(removeAssignment);
+	$('.hideInfo #complete').click(completeAssignment);
 	$('.hideInfo #cancel').click(removeAssignment);
+
 }
+
 
 
 function moreInfo(e){
@@ -22,6 +29,8 @@ function moreInfo(e){
 	$('#' + assignmentID + ' .hideInfo').fadeToggle();
 
 }
+
+
 
 function removeAssignment(){
 	console.log("clicked");
@@ -36,9 +45,22 @@ function removeAssignment(){
 }
 
 
-function completeAssignment(points){
+function completeAssignment(pts){
+		console.log("clicked");
+  	var assignmentID = $(this).closest('.assignment').attr('id');
+  	var target = $('#' + assignmentID);
+	$('#' + assignmentID + ' .removeAssignment').hide();
+	target.animate({
+    	opacity: "-=1"
+  	}, 300, function() {
+    target.remove();
+  	});
+  		console.log(pts+"___");
+	pointsEarned += parseInt(JSON.stringify(pts));
+	console.log(pointsEarned);
+  	$('#ptss').text(pointsEarned);
 
-	profilePoints = profilePoints + points;
-	//removeAssignment();
-	document.getElementById('displayPoints').innerHTML = "<h4>Points Earned: " + profilePoints + "pts</h4>";
+	// profilePoints = profilePoints + points;
+	// //removeAssignment();
+	// document.getElementById('displayPoints').innerHTML = "<h4>Points Earned: " + profilePoints + "pts</h4>";
 }
