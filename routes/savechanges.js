9d256927861d71e79c575edc239bcc5b2d7ed82d
Yongho Jun <y4jun@ucsd.edu>
2020-03-14 18:14:43 -0700

    redeem change

--- routes/savechanges.js
+++ routes/savechanges.js
@@ -13,6 +13,15 @@ exports.savechanges = function(req, res) { 
 		if(item == "goldchain"){
 		data.goldchainBought = true;
 	}
+	if(item == "puppy"){
+		data.puppyBought = true;
+	}
+	if(item == "palmtree"){
+		data.palmtreeBought = true;
+	}
+	if(item == "flower"){
+		data.flowerBought = true;
+	}
 	
 	res.render('redeem', data)
 }
